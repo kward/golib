91ccc63dc5a4ee70c16cef386c69301240c7a936
Kate Ward <kate.ward@forestent.com>
2017-09-12 00:02:18 +0200

    Added function comments.

--- math/extrema.go
+++ math/extrema.go
@@ -1,7 +1,10 @@
-// Basic max(x, y) routines that aren't provided by default.
-
+/*
+Package math provides basic constants and mathematical functions that aren't
+provided by default.
+*/
 package math
 
+// Max returns the larger of x or y.
 func Max(x, y int) int {
 	if x > y {
 		return x
@@ -9,6 +12,7 @@ func Max(x, y int) int {
 	return y
 }
 
+// Min returns the smaller of x or y.
 func Min(x, y int) int {
 	if x < y {
 		return x
