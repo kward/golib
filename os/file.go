3308c1b953ad08ab09cdca46a413f6c22b9a3230
Kate Ward <kate.ward@forestent.com>
2017-12-26 01:51:49 +0100

    Match Copy() behavior to os.Link() and os.Rename().

--- os/file.go
+++ os/file.go
@@ -11,27 +11,26 @@ import (
 	"google.golang.org/grpc/codes"
 )
 
-// Copy a file, returning the number of bytes copied.
-func Copy(oldpath, newpath string) (int64, error) {
+// Copy a file.
+func Copy(oldpath, newpath string) error {
 	src, err := os.Open(oldpath)
 	if err != nil {
-		return -1, errors.Errorf(codes.InvalidArgument, "failure opening source; %s", err)
+		return errors.Errorf(codes.InvalidArgument, "failure opening source; %s", err)
 	}
 	defer src.Close()
 
 	dest, err := os.Create(newpath)
 	if err != nil {
-		return -1, errors.Errorf(codes.InvalidArgument, "failure creating destination; %s", err)
+		return errors.Errorf(codes.InvalidArgument, "failure creating destination; %s", err)
 	}
 	defer dest.Close()
 
-	written, err := io.Copy(dest, src)
-	if err != nil {
-		return written, errors.Errorf(codes.FailedPrecondition, "failure during copy; %s", err)
+	if _, err := io.Copy(dest, src); err != nil {
+		return errors.Errorf(codes.FailedPrecondition, "failure during copy; %s", err)
 	}
 	if err := dest.Sync(); err != nil {
-		return written, errors.Errorf(codes.FailedPrecondition, "failure during sync; %s", err)
+		return errors.Errorf(codes.FailedPrecondition, "failure during sync; %s", err)
 	}
 
-	return written, nil
+	return nil
 }
