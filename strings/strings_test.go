64e896b4823e380e0ae86e84cf8d7505fd43f734
Kate Ward <kate.ward@forestent.com>
2020-02-07 05:13:56 +0100

    Modernized tests.

--- strings/strings_test.go
+++ strings/strings_test.go
@@ -1,59 +1,49 @@
 package strings
 
 import (
+	"fmt"
 	"testing"
 
 	operators "github.com/kward/golib/operators"
 )
 
 func TestSplitNMerged(t *testing.T) {
-	t.Parallel()
-
-	var got, want []string
-
-	want = []string{"1", "2", "3"}
-	got = SplitNMerged("1 2 3", " ", -1)
-	if !operators.EqualSlicesOfString(want, got) {
-		t.Errorf("SplitNMerged(): want %v, got %v", want, got)
-	}
-
-	want = []string{"1", "2", "3"}
-	got = SplitNMerged("1 2   3", " ", -1)
-	if !operators.EqualSlicesOfString(want, got) {
-		t.Errorf("SplitNMerged(): want %v, got %v", want, got)
-	}
-
-	want = []string{}
-	got = SplitNMerged("", " ", -1)
-	if !operators.EqualSlicesOfString(want, got) {
-		t.Errorf("SplitNMerged(): want %v, got %v", want, got)
+	for _, tc := range []struct {
+		str  string
+		sep  string
+		cols int
+		want []string
+	}{
+		{"1 2 3", " ", -1, []string{"1", "2", "3"}},
+		{"1 2   3", " ", -1, []string{"1", "2", "3"}},
+		{"", " ", -1, []string{}},
+	} {
+		t.Run(fmt.Sprintf("SplitNMerged()"), func(t *testing.T) {
+			got, want := SplitNMerged(tc.str, tc.sep, tc.cols), tc.want
+			if !operators.EqualSlicesOfString(want, got) {
+				t.Errorf("SplitNMerged(): want %v, got %v", want, got)
+			}
+		})
 	}
 }
 
 func TestStretch(t *testing.T) {
-	t.Parallel()
-
-	var got, have, want string
-
-	// Long.
-	have = "str1"
-	got = Stretch(have, ' ', 6)
-	want = "str1  "
-	if want != got {
-		t.Errorf("Stretch(%q): got %v, want %v", have, want, got)
-	}
-	// Just right.
-	have = "str2"
-	got = Stretch(have, ' ', 4)
-	want = "str2"
-	if want != got {
-		t.Errorf("Stretch(%q): got %v, want %v", have, want, got)
-	}
-	// Short.
-	have = "str3"
-	got = Stretch(have, ' ', 1)
-	want = "str3"
-	if want != got {
-		t.Errorf("Stretch(%q): got %v, want %v", have, want, got)
+	for _, tc := range []struct {
+		desc   string
+		str    string
+		sep    rune
+		length int
+		want   string
+	}{
+		{"long", "str1", ' ', 6, "str1  "},
+		{"just right", "str2", ' ', 4, "str2"},
+		{"short", "str3", ' ', 1, "str3"},
+		{"empty", "", ' ', 5, "     "},
+	} {
+		t.Run(fmt.Sprintf("Stretch() %s", tc.desc), func(t *testing.T) {
+			if got, want := Stretch(tc.str, tc.sep, tc.length), tc.want; got != want {
+				t.Errorf("got %q, want %q", got, want)
+			}
+		})
 	}
 }
